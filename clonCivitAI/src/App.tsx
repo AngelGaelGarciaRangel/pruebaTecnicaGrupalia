0d7825a1d0e3f30504130a9ae51c9c0de127c467
gaelgarcia003 <105666768+AngelGaelGarciaRangel@users.noreply.github.com>
2024-04-30 19:20:23 -0600

    Added cute introduction test

--- clonCivitAI/src/App.tsx
+++ clonCivitAI/src/App.tsx
@@ -48,6 +48,8 @@ function App() {
 
   return (
         <div className="container mx-auto px-4">
+            <h1 className='text-center'>CivitAI Clone</h1>
+            <p>This page is a clone of the existing <a href="https://civitai.com/">CivitAI webpage</a>, a page dedicated to share cool AI images created by his community. I am using his public <a href="https://github.com/civitai/civitai/wiki/REST-API-Reference">API</a>, to make a technical assesment for Grupalia. (Hi Grupalia Team👋) </p>
             <div className="flex space-x-4">
                 {columns.map((column, index) => (
                     <div key={index} className="flex-1">
